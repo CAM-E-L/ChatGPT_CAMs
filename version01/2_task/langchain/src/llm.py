72ad7e081bbc9b3f2d9446fa296f825fd85a09fc
Dara <dara.kamalian1@gmail.com>
2024-06-18 22:52:31 +0200

    New approach using Langchain

--- version01/2_task/langchain/src/llm.py
+++ version01/2_task/langchain/src/llm.py
@@ -3,7 +3,6 @@ from langchain_openai import ChatOpenAI
 
 
 llm = ChatOpenAI(model='gpt-4o', openai_api_key='openai_api_key', max_tokens=500)
-# I used the api_key from my personal account to test the code
 
 prompt_template = PromptTemplate(template="{initial_prompt}\n\nImage: {image_base64}", input_variables=["initial_prompt", "image_base64"])
 chain = LLMChain(prompt=prompt_template, llm=llm)
