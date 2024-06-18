097ce849e8eb156bf8d602c0bd7cedf9564fb6a4
Dara <dara.kamalian1@gmail.com>
2024-06-18 22:27:46 +0200

    New approach using Langchain, the result gets stored in a csv file in the same directory as the images

---
+++ version01/2_task/langchain/src/initial_prompt.py
@@ -0,0 +1,24 @@
+initial_prompt = """
+<Data Structure:
+In the picture you see a so called "Cognitive-Affective Map" (CAM), whereby a CAM can be considered as a specific form of mind map drawn by a person regarding a specific topic. There a different elements in the CAM, which you should consider:
+1) There a concepts (also called nodes), which are linked by connections (also called edges).
+2) Concepts incorporate an emotional evaluation by representing whether a person associates positive (green stroke), negative (red stroke), neutral (yellow stroke) or ambivalent (purple stroke) emotions with a drawn concept.
+2a) Depending on the stroke width of the positive or negative concepts the concept is emotionally evaluated as more positive or negative.
+3) Furthermore, it is possible to specify the connections in different strengths and in two different forms:
+3a) Depending on the width of the line the association between two drawn concepts is stronger.
+3b) Solid lines stand for supporting connections and dashed lines stand for inhibitory connections.>
+
+<Specific Topic:
+People had the task of reflecting on their experiences of a newly introduced "open space" office (new office). Thereby two networks side by side were drawn, the left about the past expectations regarding the new office ("damalige Erwartungen ans neue Büro"), the right about the current experiences ("aktuelles Erleben im neuen Büro").>
+
+<Task:
+1) Write a text no longer than 300 words describing the CAM, consider thereby the specific topic.
+2) Write neutrally in the third person.
+3) Start with a short introductory sentence that describes the topic.
+4) Do not explain the data structure of CAMs.
+5) Focus on the drawn concepts and not on the drawn connections.
+6) At the end provide a summary paragraph of the drawn CAM.
+7) Explain the two drawn networks separately and compare them with each other.
+8) Write in German
+>
+"""
---
+++ version01/2_task/langchain/src/save_to_csv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+from datetime import datetime
+
+def save_to_csv(directory, descriptions):
+    now = datetime.now()
+    print(now)
+    df = pd.DataFrame(descriptions)
+    df.columns = ["CAM Name", "Beschreibung"]
+    csv_path = os.path.join(directory, f"CAM_Beschreibung_{now}.csv")
+    df.to_csv(csv_path, index=False, sep=';', encoding='utf-8')
